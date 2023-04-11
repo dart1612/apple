6bf72ead0855ad20624790839588153f55043013
dart1612 <121093111+dart1612@users.noreply.github.com>
2023-04-11 16:48:25 +0530

    Update AppleController.java

--- src/main/java/com/example/AppleController.java
+++ src/main/java/com/example/AppleController.java
@@ -7,7 +7,7 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;   
 
 /**
  *
